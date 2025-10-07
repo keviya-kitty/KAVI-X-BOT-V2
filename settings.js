48a13b1fad2f9340a8abe7bff548d0a872c20e9a
keviya-kitty <kevinshavindu11@gmail.com>
2025-10-07 17:54:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.sessionID = process.env.SESSIONID || ""
-global.mongodburi = process.env.MONGODB_URI || ""
+global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~H8ASiD6J#KZLPOA2yqx2_HZd9aGM_KiYtZbH35vfpgLSYkx0UC7I"
+global.mongodburi = process.env.MONGODB_URI || "mongodb+srv://kevinofficial20090408:kevinofficial20090408@cluster0.p0tz5io.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 global.botname = process.env.BOTNAME || "KAVI-X MD"
 global.ownernumber = process.env.OWNERNUMBER || "94766577249"
 global.ownername = process.env.OWNERNAME || "Cyber Kavi"
